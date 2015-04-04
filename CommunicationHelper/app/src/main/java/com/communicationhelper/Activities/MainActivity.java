b674d96da041efcffdbd5bb1cd42d9b12c1a0cf7
Maria Kharkova <maria.kharkova2011@gmail.com>
2015-04-04 22:02:10 +0300

    Recognize voice

--- CommunicationHelper/app/src/main/java/com/communicationhelper/Activities/MainActivity.java
+++ CommunicationHelper/app/src/main/java/com/communicationhelper/Activities/MainActivity.java
@@ -5,26 +5,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.os.Build;
+import android.widget.ImageButton;
 
 import com.communicationhelper.R;
+import ru.yandex.speechkit.*;
 
 import com.communicationhelper.Conversation.ConversationFragment;
 
 
-public class MainActivity extends ActionBarActivity {
-    AutoCompleteTextView mAutocomplete;
 
+public class MainActivity extends ActionBarActivity implements RecognizerListener, View.OnClickListener {
+    AutoCompleteTextView mAutocomplete;
+    private static final String TAG = "SpeechKitSample";
+    private static final String TAGRESULT = "RESULT";
+    private ImageButton btn_micro;
+    private ImageButton btn_edit;
+    private Recognizer recognizer;
+    private String result_answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SpeechKit.getInstance().configure(getBaseContext(), "6afff325-1614-4958-9f6c-700ef26e566a");
         setContentView(R.layout.activity_main);
+        btn_micro = (ImageButton) findViewById(R.id.btn_micro);
+        btn_edit = (ImageButton) findViewById(R.id.text_button);
+        recognizer = null;
+        btn_micro.setOnClickListener(this);
+        btn_edit.setOnClickListener(this);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new ConversationFragment())
@@ -75,6 +91,84 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRecordingBegin(Recognizer recognizer) {
+        Log.v(TAG, "onRecordingBegin");
+
+    }
+
+    @Override
+    public void onSpeechDetected(Recognizer recognizer) {
+        Log.v(TAG, "onSpeechDetected");
+
+    }
+
+    @Override
+    public void onRecordingDone(Recognizer recognizer) {
+        Log.v(TAG, "onRecordDone");
+
+
+    }
+
+    @Override
+    public void onSoundDataRecorded(Recognizer recognizer, byte[] bytes) {
+        Log.v(TAG, "onSoundDataRecorded");
+    }
+
+    @Override
+    public void onPowerUpdated(Recognizer recognizer, float power) {
+//        Log.v(TAG, "onPoverUpdate");
+//        Log.v("onPoverUpdate", String.valueOf(power));
+//        if (power<0.1){
+//            recognizer.finishRecording();
+//            Log.v("onPoverUpdate", "<0.3");
+//        }
+//        else
+//        if(power>=0.1){
+//            Log.v("onPoverUpdate", ">0.3");
+////            recognizer = Recognizer.create("ru-RU", "general", new MainActivity());
+////            recognizer.setVADEnabled(false);
+////            recognizer.start();
+//        }
+
+    }
+
+    @Override
+    public void onPartialResults(Recognizer recognizer, Recognition recognition, boolean b) {
+        Log.v(TAGRESULT, recognition.getBestResultText());
+        Log.v(TAG, "onPartialResult");
+
+    }
+
+    @Override
+    public void onRecognitionDone(Recognizer recognizer, Recognition recognition) {
+        Log.v(TAG, "onRecordingDone");
+        result_answer = recognition.getBestResultText();
+        Log.v("recognized_text", result_answer);
+
+    }
+
+    @Override
+    public void onError(Recognizer recognizer, ru.yandex.speechkit.Error error) {
+        Log.v(TAG, "onError");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_micro:
+                recognizer = Recognizer.create("ru-RU", "general", new MainActivity());
+                recognizer.setVADEnabled(false);
+                recognizer.start();
+                break;
+            case R.id.text_button:
+                recognizer.finishRecording();
+                break;
+        }
     }
 
     public void executeTypeText(View view) {
